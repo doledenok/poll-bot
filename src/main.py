8b6ec72c3a1c60af6aaf962470cc5b7298c53edf
Max Doledenok <doledenok@ispras.ru>
2024-04-18 21:19:52 +0300

    add ConversationHandler for bot

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import os
 import sys
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
-from admin import admin_main
-from user import user_main
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler, ConversationHandler
+from admin import admin_main, admin_states
+from user import user_main, user_states
 
+
+# First state after start
+CHOOSING_ROLE = 0
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """
@@ -12,38 +15,43 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """
     keyboard = [
         [
-            InlineKeyboardButton("Admin", callback_data="admin"),
-            InlineKeyboardButton("User", callback_data="user"),
+            InlineKeyboardButton("Create exam", callback_data="admin_start"),
+            InlineKeyboardButton("Join exam", callback_data="user_start"),
         ],
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    await update.message.reply_text("Please choose your role:", reply_markup=reply_markup)
+    await update.message.reply_text("Please choose the scenario:", reply_markup=reply_markup)
+    return CHOOSING_ROLE
 
 
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """
-    Parses the callback data from pressing some buttons.
-    """
-    query = update.callback_query
-
-    if query.data == "admin":
-        await admin_main(update, context)
-    elif query.data == "user":
-        await user_main(update, context)
-    else:
-        # CallbackQueries need to be answered, even if no notification to the user is needed
-        await query.answer()
-
-
-if __name__ == "__main__":
+def main():
     if not (token := os.environ.get("TELEGRAM_POLL_BOT_TOKEN")):
         print("Can't find telegram token! Please set TELEGRAM_POLL_BOT_TOKEN environment variable.")
         sys.exit(1)
 
     application = ApplicationBuilder().token(token).build()
-    application.add_handler(CommandHandler('start', start))
-    application.add_handler(CallbackQueryHandler(button))
+
+    states = {
+            CHOOSING_ROLE: [
+                CallbackQueryHandler(admin_main, pattern="^admin*"),
+                CallbackQueryHandler(user_main, pattern="^user*"),
+            ],
+        }
+    states |= admin_states | user_states
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states=states,
+        fallbacks=[CommandHandler("start", start)],
+    )
+
+    # Add ConversationHandler to application that will be used for handling updates
+    application.add_handler(conv_handler)
 
     application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
--- src/user.py
+++ src/user.py
@@ -1,2 +1,50 @@
-def user_main():
-    pass
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    CallbackQueryHandler,
+    ConversationHandler,
+    filters,
+    MessageHandler,
+)
+
+
+USER_STATES_BASE = 100
+
+(
+    USER_MAIN,
+    USER_NAME,
+    USER_INPUT_ID,
+    USER_LIST_OF_PARTICIPANTS,
+    USER_CHOOSE_RATE,
+    USER_RATE_CALMNESS_STORY,
+    USER_RATE_CALMNESS_QUESTIONS,
+    USER_RATE_EYE_CONTACT_STORY,
+    USER_RATE_EYE_CONTACT_QUESTIONS,
+    USER_RATE_ANSWERS_SKILL,
+    USER_RATE_NOTES,
+    USER_FINISH_EXAM,
+) = range(USER_STATES_BASE, USER_STATES_BASE + 12)
+
+
+async def user_main(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    if query.data == "user_start":
+        update.message.reply_text("Please enter your name:")
+        await user_name(update, context)
+        return USER_NAME
+    #elif query.data == "user_name":
+    #    await user_set_name(update, context)
+    await query.answer("Not implemented yet")
+
+
+async def user_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    name = update.message.text
+    print(f"GOT NAME: {name}")
+
+
+user_states = {
+    USER_MAIN: [MessageHandler(filters.ALL, user_main)],
+    # TODO
+}
